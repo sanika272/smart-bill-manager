13843865fc462b7ff0862cd38cb164d16dc2ed87
sanika272 <bakaresanika@gmail.com>
2026-03-20 18:06:17 +0530

    fixed register path issue

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Dashboard from "./pages/DashBoard";
 import AddBill from "./pages/AddBill";
 import Bills from "./pages/Bills";
